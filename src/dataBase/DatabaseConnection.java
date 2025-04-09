9ad2016179e9ff94593c8ba2f7af6d5e4c7f01e3
Crisbrix <cmateoramirez@ucundinamarca.edu.co>
2025-04-09 10:39:15 -0500

    se arreglo la conexión de la base de datos

--- src/dataBase/DatabaseConnection.java
+++ src/dataBase/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/quality_docs";
-    private static final String USER = "tu_usuario";
-    private static final String PASSWORD = "tu_contraseña";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
